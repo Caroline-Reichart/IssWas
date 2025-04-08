482b9d2413e2f351d1d9b29192faf5c6fb452c32
ARdcistudent <andre.ruehmling@dci-student.org>
2025-04-08 11:31:54 +0200

    Add Popper.js dependency and update Navbar styles; remove Tailwind configuration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Navbar = ()=>{
     return(
         <header>
-	        <nav class="navbar navbar-expand-lg fixed-top navbarScroll navbar-dark">
+	        <nav class="navbar navbar-expand-lg fixed-top navbarScroll navbar-dark" style={{backgroundColor: "#000000"}}>
                 <div class="container">
   	            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'popper.js/dist/umd/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
