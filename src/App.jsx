5b0dfbbd4ad1fc7c8ec5669d42ef0d3deeff11f5
caroline reichart <caroline.reichhart@dci-student.com>
2025-04-10 07:37:54 +0200

    Refactor App-Komponente: Implementiere AppWrapper mit bedingter Navbar-Anzeige und aktualisiere Routing für neue Seiten

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navbar } from './components/Navbar';
+/* import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { Content } from './components/Content';
 import { Routes, Route } from 'react-router-dom';
@@ -23,4 +23,39 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
+
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Content } from './components/Content';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Fruehstueck from './pages/Fruehstueck';
+import Mittag from './pages/Mittag';
+import Abend from './pages/Abend';
+import Cocktails from './pages/Cocktails';
+
+function AppWrapper() {
+  const location = useLocation();
+
+  // Liste der Seiten, auf denen die Navbar NICHT angezeigt werden soll
+  const hideNavbarPaths = ['/Fruehstueck', '/Mittag', '/Abend', '/Cocktails'];
+
+  const showNavbar = !hideNavbarPaths.includes(location.pathname);
+
+  return (
+    <div className="body-container">
+      {showNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Content />} />
+        <Route path="/Fruehstueck" element={<Fruehstueck />} />
+        <Route path="/Mittag" element={<Mittag />} />
+        <Route path="/Abend" element={<Abend />} />
+        <Route path="/Cocktails" element={<Cocktails />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default AppWrapper;
+
