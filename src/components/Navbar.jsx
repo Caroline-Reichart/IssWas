bb45bcdf4fe47d6d5642c79ebe4e696b5c115bcd
caroline reichart <caroline.reichhart@dci-student.com>
2025-04-10 09:32:41 +0200

    Ändere den Pfad des Logos in der Navbar-Komponente für korrekte Bildanzeige

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export const Navbar = ()=>{
     return(
         <header>
             <nav className="navbar navbar-expand-lg fixed-top navbarScroll navbar-dark" style={{backgroundColor: "#000000"}}>
-            <img src="../src/assets/logo_issWas.webp" alt="logo" style={{width:"100px", display:"inline", marginLeft:"50px", borderRadius:"10px"}}/>
+            <img src="/assets/logo_issWas.webp" alt="logo" style={{width:"100px", display:"inline", marginLeft:"50px", borderRadius:"10px"}}/>
                 <div className="container">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
